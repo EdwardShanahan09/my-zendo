d73d9c5f4f34bce951df485f73ebbca4f9818830
Edward Shanahan <edwardshanahan79@gmail.com>
2024-10-09 20:38:00 +0100

    Add content to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,43 @@
+import Logo from "../../assets/icons/logo.svg";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-secondary px-4 md:px-6 lg:px-16 py-6 text-dark">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+        {/* Left Column - Logo, Developer Info, and Copyright */}
+        <div className="flex flex-col items-start text-sm">
+          <img
+            className="w-[30px] md:w-[40px] mb-4"
+            src={Logo}
+            alt="Created by archer7 from the Noun Project"
+          />
+          <p className="mb-2">
+            Designed and developed by:{" "}
+            <a className="hover:text-primary" href="#">
+              Edward Shanahan
+            </a>
+          </p>
+          <p>&copy; My Zendo 2024</p>
+        </div>
+
+        {/* Right Column - Links */}
+        <div className="md:text-right">
+          <ul className="inline-flex space-x-4">
+            <li>
+              <a className="text-dark hover:text-primary text-sm" href="#">
+                Login
+              </a>
+            </li>
+            <li>
+              <a className="text-dark hover:text-primary text-sm" href="#">
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
