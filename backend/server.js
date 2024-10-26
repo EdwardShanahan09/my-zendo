2f21ac2bfcd631be2eb05d2cf7a08e77b1bae0e1
Edward Shanahan <edwardshanahan79@gmail.com>
2024-10-26 13:40:57 +0100

    Connect to mongoose server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
@@ -7,11 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log("Mongo URI:", process.env.MONGO_URL);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
