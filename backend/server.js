53722d4c4806d9818845bd711972c9a42be37326
Edward Shanahan <edwardshanahan79@gmail.com>
2024-10-28 16:17:06 +0000

    Add User schema

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const User = require("./Models/User/User");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-console.log("Mongo URI:", process.env.MONGO_URL);
 
 // MongoDB connection
 mongoose
@@ -18,6 +18,20 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const newUser = new User({ username, email, password });
+
+    await newUser.save();
+
+    res.status(201).send("User created successfully!");
+  } catch (error) {
+    res.status(400).send("Error creating user:" + error.message);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
