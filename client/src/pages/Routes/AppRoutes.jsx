b516f62302face52df78c56915f578207ffe087f
Edward Shanahan <edwardshanahan79@gmail.com>
2024-12-10 17:39:35 +0000

    Add ProtectedRoute component

--- client/src/pages/Routes/AppRoutes.jsx
+++ client/src/pages/Routes/AppRoutes.jsx
@@ -2,7 +2,9 @@ import AuthLayout from "../../layouts/AuthLayout";
 import MainLayout from "../../layouts/MainLayout";
 import Login from "../Auth/Login/Login";
 import Signup from "../Auth/Signup/SignUp";
+import Dashboard from "../Dashboard/Dashboard";
 import Home from "../Home/Home";
+import ProtectedRoute from "../Routes/ProtectedRoute";
 import { Routes, Route } from "react-router-dom";
 
 const AppRoutes = () => {
@@ -15,6 +17,14 @@ const AppRoutes = () => {
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
       </Route>
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      ></Route>
     </Routes>
   );
 };
